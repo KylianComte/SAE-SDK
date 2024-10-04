fcbf69e9fd070359264c88b5541a7283fa58c728
Kylian <kylian.comte@etu.iut-tlse3.fr>
2024-10-04 13:30:26 +0000

    Debut interface

--- Instrument.py
+++ Instrument.py
@@ -4,11 +4,11 @@ class Instrument ():
     def __init__(self, name, type, adress) :
         self.name = name
         self.type = type
-        self.ipaddr = adress
+        self.addr = adress
         self.instrument = None
 
     def connect(self) :
-        self.instrument = pyvisa.ResourceManager().open_resource(self.ipaddr) # Name ?
+        self.instrument = pyvisa.ResourceManager('@sim').open_resource(self.addr) # Name in sim librairy
         print(self.instrument.query('*IDN?'))
 
     def preset(self) :
--- SAE_SDK.py
+++ SAE_SDK.py
@@ -1,22 +1,54 @@
 # Script to run the SAE SDK
 
 #from .Mesure import Measure
-#from .Instrument import Arv
 #from .Report import Report
 #from .Resultats import Resultats
 
 # Imports
 import pyvisa
+from Instrument import Instrument, Arv
 
 
 #0. Find instruments
 
 def instrumentscan() :
     resourceManager = pyvisa.ResourceManager('@sim')
-    print(resourceManager.list_resources())
+    return resourceManager.list_resources()
 
 
 
 ### Main ###
-instrumentscan()
+print("Bienvenue dans le SAE SDK")
+print("Programme créé par RUSSO Martin et COMTE Kylian")
+print("Pour commencer, veuillez brancher les instruments et appuyer sur entrée")
+input()
+print("Recherche des instruments...")
+ListeInstruments = instrumentscan()
+print("Instruments trouvés :")
+for i in range(len(ListeInstruments)) :
+    print(f'{i}. {ListeInstruments[i]}')
+
+print("Veuillez choisir l'instrument à utiliser (entrez un nombre entre 0 et " + str(len(ListeInstruments)-1) + ")")
+
+no_instrument = int(input())
+while (no_instrument < 0 or no_instrument >= len(ListeInstruments)) :
+    print("Entrée invalide : Veuillez choisir un nombre entre 0 et " + str(len(ListeInstruments)-1))
+    no_instrument = int(input())
+
+print("Vous avez choisi l'instrument " + ListeInstruments[no_instrument])
+print("L'instrument est-il un ARV ? (O/N)")
+is_arv = input()
+while (is_arv != 'O' and is_arv != 'N') :
+    print("Entrée invalide : Veuillez entrer O ou N")
+    is_arv = input()
+if (is_arv == 'O') :
+    print("Nommez l'instrument")
+    nom_instrument = input()
+    instrument = Arv(nom_instrument, ListeInstruments[no_instrument])
+else :
+    print("Type d'instrument non pris en compte, modifiez le programme")
+    exit()
+
+print("Connexion à l'instrument...")
+instrument.connect()
 
