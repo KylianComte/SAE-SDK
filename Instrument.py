f683d0a81dd6366c38fb1275c1c8b4f279da4c16
Kylian <kylian.comte@etu.iut-tlse3.fr>
2024-10-04 13:46:16 +0000

    gestion exception connection impossible

--- Instrument.py
+++ Instrument.py
@@ -8,8 +8,14 @@ class Instrument ():
         self.instrument = None
 
     def connect(self) :
-        self.instrument = pyvisa.ResourceManager('@sim').open_resource(self.addr) # Name in sim librairy
-        print(self.instrument.query('*IDN?'))
+        try :
+            self.instrument = pyvisa.ResourceManager('@sim').open_resource(self.addr) # Name in sim librairy
+            description = self.instrument.query('*IDN?')
+        except :
+            print("Erreur lors de la connexion, veuillez relancer le programme")
+            exit()
+        print("Instrument connecté avec succès !")
+        print(description)
 
     def preset(self) :
         pass
