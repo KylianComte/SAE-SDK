2a85d67d481b4d438c2c9a57349d8c0cd15053b7
Martin Russo <martin.russo@etu.iut-tlse3.fr>
2024-10-10 20:24:11 +0200

    report1

--- Report.py
+++ Report.py
@@ -2,16 +2,18 @@ import reportlab
 from reportlab.lib.pagesizes import letter, A4
 from reportlab.pdfgen import canvas
 from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import Paragraph, SimpleDocTemplate
+import datetime
 
 class Report ():
-    def __init__(self, nomtechnicien, filename, titredoc, date, resultat) :
+    def __init__(self, nomtechnicien, filename) :
         self.nomtechnicien = nomtechnicien
         self.filename = filename
         self.pdf = canvas.Canvas(self.filename, pagesize=letter)
         self.width, self.height = letter
-        self.titredoc = titredoc
-        self.date = date
-        self.resultat = resultat
+        self.titredoc = "Certificat de conformité"
+        self.date = datetime.datetime.now().strftime("%d-%m-%Y / %H : %M")
 
     def title(self) :
         self.pdf.setFillColorRGB(255,0,0)
@@ -24,22 +26,30 @@ class Report ():
 
         self.pdf.setFillColorRGB(0,0,255)
         self.pdf.setFont("Helvetica-Bold", 12)
-        self.pdf.drawCentredString(self.width/2, self.height - 120, self.date)
+        self.pdf.drawString(self.width/14, self.height - 150, "Ce rapport a été rédigé le " + str(self.date)[:10] + " à " + str(self.date)[-7:])
 
     def content(self):
         self.pdf.setFillColorRGB(0,0,0)
         self.pdf.setFont("Helvetica-Bold", 12)
-        self.pdf.drawText("Nom de l'instrument : ")
-        self.pdf.drawText("Mesure de l'instrument : " )
+        self.pdf.drawString(self.width/14, self.height - 180,"Nom de l'instrument : ")
+        self.pdf.drawString(self.width/14, self.height - 200,"Mesure de l'instrument : " )
 
     def save(self):
         self.pdf.save()
 
     def create_pdf(self):
-       self.title()
-       self.save()
+        self.title()
+        self.content()
+        self.save()
+       
+resultat = {"name" : "ARV2", 
+            "type_mesure" : "Coefficien de réflexion (S11)", 
+            "type_instrument" : "ARV",
+            "resultat" : "S11 = 25dB",
+            "f0" : "1GHz"
+            }
 
-mondoc = Report("Martin Russo & Kylian Comte", "Mesures.pdf", "Rapport de mesure", "08/10/2024")
+mondoc = Report("Martin Russo & Kylian Comte", "Mesures.pdf")
 mondoc.create_pdf()
        
 
