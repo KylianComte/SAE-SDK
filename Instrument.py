98076e633a693b2f3c0f526b8394a37c97a2fb57
Kylian <kylian.comte@etu.iut-tlse3.fr>
2024-10-11 07:16:33 +0000

    Refactor code to prompt user for calibration type and perform calibration

--- Instrument.py
+++ Instrument.py
@@ -32,7 +32,7 @@ class Arv(Instrument) :
         print(self.instrument.query('*RST'))
 
     def calibration(self):
-        #TODO : set frequencies (min, max, step)
+        
         print("Garder les valeurs par défaut ? (O/N)")
         keep = input()
         while (keep != 'O' and keep != 'N') :
@@ -46,25 +46,62 @@ class Arv(Instrument) :
             fmax = int(input())
             print("Pas de fréquence :(MHz)")
             fstep = int(input())
-            #TODO : set power (ref, div)
+            #TODO : set frequencies (min, max, step)
+            
             print("Puissance de référence :(dBm)")
             pref = int(input())
             print("Puissance par division :(dB)")
             pdiv = int(input())
-            #TODO : set number of points
+            #TODO : set power (ref, div)            
+            
             print("Nombre de points :")
             n = int(input())
+            #TODO : set number of points
+
         else :
             print("Valeurs par défaut conservées, appareil prêt à être calibré")
 
         
         print("Quelle calibration voulez-vous effectuer ?")
-        print("1. Calibration x")
-        print("2. Calibration x")
-        print("3. Calibration x")
-        input()
-        print(self.instrument.query('!CAL'))
-        #TODO : calibrate
+        print("1. Calibration Open Response")
+        print("2. Calibration Short Response")
+        print("3. Calibration Thru Response")
+        print("4. Calibration Full 1 port")
+        print("5. Calibration 1 path 2 port")
+        choix = int(input())
+        while (choix < 1 or choix > 5) :
+            print("Entrée invalide : Veuillez choisir un nombre entre 1 et 5")
+            choix = int(input())
+        if (choix == 1) :
+            print("Connecter l'open puis appuyer sur entrée")
+            input()
+            print(self.instrument.query('!CAL')) #TODO : check if it's the right command
+        elif (choix == 2) :
+            print("Connecter le short puis appuyer sur entrée (p1)")
+            input()
+            print(self.instrument.query('!CAL'))
+        elif (choix == 3) :
+            print("Connecter le thru puis appuyer sur entrée (p1)")
+            input()
+            print(self.instrument.query('!CAL'))
+        elif (choix == 4) :
+            print("Connecter l'open puis appuyer sur entrée (p1)")
+            input()
+            print("Connecter le short puis appuyer sur entrée (p1)")
+            input()
+            print("Connecter le load puis appuyer sur entrée (p1)")
+            input()
+            print(self.instrument.query('!CAL'))
+        elif (choix == 5) :
+            print("Connecter l'open puis appuyer sur entrée (p1)")
+            input()
+            print("Connecter le short puis appuyer sur entrée (p1)")
+            input()
+            print("Connecter le load puis appuyer sur entrée (p1)")
+            input()
+            print("Connecter le thru puis appuyer sur entrée (p1)")
+            input()
+            print(self.instrument.query('!CAL'))
 
         print("Calibration terminée")
 
